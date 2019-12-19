cadcfce346b705135e8ee3d6c9335adb69b14d4d
rdg1g16 <rdg1g16@ecs.soton.ac.uk>
2019-12-19 17:42:28 +0000

    Updates

---
+++ precache-manifest.3fb8a34e500bde1e05e15092057d2db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dfefce82ea8e8500cbc27bcd123f6c",
+    "url": "/ChristmasChallenge/index.html"
+  },
+  {
+    "revision": "3dfae439a49ee8a8d628",
+    "url": "/ChristmasChallenge/static/css/2.8ff545c9.chunk.css"
+  },
+  {
+    "revision": "d1845fcf6615f9e3e758",
+    "url": "/ChristmasChallenge/static/css/main.101b2764.chunk.css"
+  },
+  {
+    "revision": "3dfae439a49ee8a8d628",
+    "url": "/ChristmasChallenge/static/js/2.6c583347.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ChristmasChallenge/static/js/2.6c583347.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1845fcf6615f9e3e758",
+    "url": "/ChristmasChallenge/static/js/main.e6e7e4a9.chunk.js"
+  },
+  {
+    "revision": "7f6408a560f6a417e112",
+    "url": "/ChristmasChallenge/static/js/runtime-main.be21b5f9.js"
+  }
+]);
